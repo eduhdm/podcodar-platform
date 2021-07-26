25ea08a384cb03d4bf58fda4f3a67f4e4305b0ab
Victor Hugo Souza <victorhugoams@gmail.com>
2021-07-26 13:15:12 -0300

    Feature: criada estrutura de navegação interna no app (componente de menu SideNavigationMenu).

---
+++ client/src/components/menus/SideNavigationMenu.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { Typography, Button } from '@material-ui/core'
+import { useHistory } from 'react-router-dom'
+import menuRoutes from './SideNavigationRoutes'
+
+function SideNavigationMenu(): JSX.Element {
+  const browserHistory = useHistory()
+
+  const navigateTo = (path: string): void => {
+    browserHistory.push(`/${path}`)
+  }
+
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        left: 60,
+        top: 130,
+        borderRadius: 5,
+        borderColor: '#5D417A',
+        backgroundColor: '#855fad',
+        padding: 10,
+        minHeight: 800,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+      }}
+    >
+      <div style={{ justifySelf: 'start', paddingBottom: 15 }}>
+        <Typography
+          style={{
+            fontFamily: 'Quantico',
+            fontSize: 40,
+            color: 'white',
+            textAlign: 'center',
+          }}
+        >
+          NAVEGAÇÃO
+        </Typography>
+      </div>
+      {menuRoutes.map((item) => (
+        <div style={{ margin: 15 }}>
+          <Button
+            color="primary"
+            variant="contained"
+            style={{
+              backgroundColor: '#f76bdf',
+              color: 'black',
+              fontWeight: 'bold',
+              width: 300,
+              height: 80,
+            }}
+            onClick={(): void => navigateTo(item.route)}
+          >
+            <Typography
+              style={{
+                fontFamily: "'Press Start 2P'",
+                color: 'white',
+                fontWeight: 'bold',
+                fontSize: 17,
+              }}
+            >
+              {item.title}
+            </Typography>
+          </Button>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default SideNavigationMenu
